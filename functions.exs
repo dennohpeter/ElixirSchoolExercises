d1c148c8a0413f69da34e44cce9b039b8c82daf5
Dennis Peterson <denispeterson96@gmail.com>
2019-04-24 10:45:24 +0300

    done with functions

--- functions.exs
+++ functions.exs
@@ -113,3 +113,100 @@ Hello, Fred Katiwa
 
 Private Functions # When we don’t want other modules accessing a specific function we can make the function private
 # We define them in Elixir with defp
+iex(27)> defmodule Greeter do
+...(27)> def hello(name), do: phrase() <> name
+...(27)> defp phrase, do: "Hello, "
+...(27)> end
+iex(28)> Greeter.hello("Mutua")
+"Hello, Mutua"
+iex(29)>
+Guards  #  Once Elixir has matched a function any existing guards will be tested.
+iex(30)> defmodule Greeter do
+...(30)> def hello(names) when is_list(names)do
+...(30)> names
+...(30)> |> Enum.join(", ")
+...(30)> |> hello
+...(30)> end
+...(30)>
+...(30)> def hello(name) when is_binary(name)do
+...(30)> phrase() <> name
+...(30)> end
+...(30)> defp phrase, do: "Hello,  "
+...(30)> end
+iex(31)> Greeter.hello(["Sean", "Steve"])
+"Hello,  Sean, Steve"
+iex(32)> Greeter.hello(["Sean", "Dennis"])
+"Hello,  Sean, Dennis"
+iex(33)>
+Default Arguments  # If we want a default value for an argument we use the argument \\ value syntax:
+iex(1)> defmodule Greeter do
+...(1)> def hello(name, language_code \\ "en") do
+...(1)> phrase(language_code) <> name
+...(1)> end
+...(1)> defp phrase("en"), do: "Hello, "
+...(1)> defp phrase("es"), do: "Hola, "
+...(1)> end
+iex(2)> Greeter.hello("Sean", "en")
+"Hello, Sean"
+iex(3)> Greeter.hello("Sean", "es")
+"Hola, Sean"
+iex(4)> Greeter.hello("Sean")
+"Hello, Sean"
+iex(5)>
+
+// Combining guards and default arguments
+// we run into problems Like
+iex(1)> defmodule Greeter do
+...(1)> def hello(names, lang_code \\ "en") when is_list(names) do
+...(1)> names
+...(1)> |> Enum.join(", ")
+...(1)> |> hello(lang_code)
+...(1)> end
+...(1)> def hello(names, lang_code \\ "en") when is_binary(names) do
+...(1)> phrase(lang_code) <> names
+...(1)> end
+...(1)> defp phrase("en"), do: "Hello, "
+...(1)> defp phrase("es"), do: "Hola, "
+...(1)> end
+** (CompileError) iex:7: def hello/2 defines defaults multiple times. Elixir allows defaults to be declared once per definition. Instead of:
+
+    def foo(:first_clause, b \\ :default) do ... end
+    def foo(:second_clause, b \\ :default) do ... end
+
+one should write:
+
+    def foo(a, b \\ :default)
+    def foo(:first_clause, b) do ... end
+    def foo(:second_clause, b) do ... end
+
+    iex:7: (module)
+iex(1)>
+// to mitigate this we use header function with our default value
+// like
+iex(3)> defmodule Greeter do
+...(3)> def hello(names, lang_code \\ "en")
+...(3)>
+...(3)> def hello(names, lang_code) when is_list(names) do
+...(3)> names
+...(3)> |> Enum.join(", ")
+...(3)> |> hello(lang_code)
+...(3)> end
+...(3)>
+...(3)> def hello(name, lang_code) when is_binary(name) do
+...(3)> phrase(lang_code) <> name
+...(3)> end
+...(3)>
+...(3)> defp phrase("en"), do: "Hello, "
+...(3)> defp phrase("es"), do: "Hola, "
+...(3)> end
+{:module, Greeter,
+ <<70, 79, 82, 49, 0, 0, 6, 92, 66, 69, 65, 77, 65, 116, 85, 56, 0, 0, 0, 164,
+   0, 0, 0, 18, 14, 69, 108, 105, 120, 105, 114, 46, 71, 114, 101, 101, 116,
+   101, 114, 8, 95, 95, 105, 110, 102, 111, 95, ...>>, {:phrase, 1}}
+iex(4)> Greeter.hello []
+"Hello, "
+iex(5)> Greeter.hello ["Dennis", "Steve","Wayua"]
+"Hello, Dennis, Steve, Wayua"
+iex(6)> Greeter.hello ["Dennis", "Steve","Wayua"], "es"
+"Hola, Dennis, Steve, Wayua"
+iex(7)>
