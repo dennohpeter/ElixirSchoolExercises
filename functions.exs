c16f7ab3e4f22f6110c58197e5fe9391ca9f224e
Dennis Peterson <denispeterson96@gmail.com>
2019-04-24 05:32:40 +0300

    Named Functions

--- functions.exs
+++ functions.exs
@@ -16,4 +16,23 @@ iex(6)> sum.(1,6)
 7
 iex(7)>
 
-// Pattern Matching 
+Pattern Matching  //Elixir uses pattern matching to check through all possible match options and select the first matching option to run:
+iex(9)> handle_result = fn
+...(9)> {:ok, result} -> IO.puts "Handling result"
+...(9)> {:ok, _} -> IO.puts "This will never run as previous will be matched beforehand."
+...(9)> {:error} -> IO.puts "An error has occurred!"
+...(9)> end
+warning: variable "result" is unused
+  iex:10
+
+#Function<6.128620087/1 in :erl_eval.expr/5>
+iex(10)> some_res = 2
+2
+iex(11)> handle_result.({:ok, some_res})
+Handling result
+:ok
+iex(12)> handle_result.({:error})
+An error has occurred!
+:ok
+iex(13)
+// Named Functions
