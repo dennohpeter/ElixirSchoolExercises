2c0282402ab25b6741a3fc662cfc2049a35a18c6
Dennis Peterson <denispeterson96@gmail.com>
2019-04-24 06:06:33 +0300

    Functions and Pattern Matching

--- functions.exs
+++ functions.exs
@@ -35,4 +35,49 @@ iex(12)> handle_result.({:error})
 An error has occurred!
 :ok
 iex(13)
-// Named Functions
+Named Functions   // Functions with names so we can easily refer to them later
+iex(14)> defmodule Greeter do
+...(14)> def hello(name)do
+...(14)> "Hello, " <> name
+...(14)> end
+...(14)> end
+{:module, Greeter,
+ <<70, 79, 82, 49, 0, 0, 4, 124, 66, 69, 65, 77, 65, 116, 85, 56, 0, 0, 0, 141,
+   0, 0, 0, 15, 14, 69, 108, 105, 120, 105, 114, 46, 71, 114, 101, 101, 116,
+   101, 114, 8, 95, 95, 105, 110, 102, 111, 95, ...>>, {:hello, 1}}
+iex(15)> Greeter.hello("Dennis")
+"Hello, Dennis"
+iex(16)>
+// in one line
+iex(17)> defmodule Greeter do
+...(17)> def hello(name), do: "hello, " <> name
+...(17)> end
+iex(18)> Greeter.hello("Peterson")
+"hello, Peterson"
+iex(19)>
+// Recursion using named functions
+iex(1)> defmodule Length do
+...(1)> def of([]), do: 0
+...(1)> def of([_ | tail]), do: 1 + of(tail)
+...(1)> end
+iex(2)> Length.of []
+0
+iex(3)> Length.of [1,2,3]
+3
+iex(4)> Length.of [1,2,3, 6]
+4
+iex(5)>
+//Function Naming and Arity
+iex(6)> defmodule Greeter2 do
+...(6)> def hello(), do: "Hello anonymous person!"
+...(6)> def hello(name), do: "Hello #{name}"
+...(6)> def hello(name1, name2), do: "Hello, #{name1} and #{name2}"
+...(6)> end
+iex(7)> Greeter2.hello()
+"Hello anonymous person!"
+iex(8)> Greeter2.hello("Dennis")
+"Hello Dennis"
+iex(9)> Greeter2.hello("Dennis", "Karim")
+"Hello, Dennis and Karim"
+iex(10)>
+//Functions and Pattern Matching
