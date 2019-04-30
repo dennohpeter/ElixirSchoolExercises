f952ee87d6bd947e924bf58d8dfce3ef04deb30f
Dennis Peterson <denispeterson96@gmail.com>
2019-04-30 14:15:34 +0300

    done with basics

---
+++ iexHelpers.ex
@@ -0,0 +1,9 @@
+defmodule IExHelpers do
+  def whats_this?(term) when is_binary(term), do: "Type: Binary"
+  def whats_this?(term) when is_nil(term), do: "Type: Nil"
+  def whats_this?(term) when is_integer(term), do: "Type: Integer"
+  def whats_this?(term) when is_float(term), do: "Type: Float"
+  def whats_this?(term) when is_boolean(term), do: "Type: Boolean"
+  def whats_this?(term) when is_atom(term), do: "Type: Atom"
+  def whats_this?(_term), do: "Type: Unknown"
+end
